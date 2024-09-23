e7961d6ee2e4554ef3ab690316834bd1499468e4
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2024-09-23 17:55:37 -0600

    First Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let car =  {
+    make : "Toyota", 
+    model: "RAV 4", 
+    year : 2021, 
+    start: function(){
+        console.log("The Car is starting")
+    }, 
+    getDetails: function(){
+        return `The car make is ${this.make} and the year is ${this.year}`
+    }
+}
+
+car.start()
+
+console.log(car.getDetails())
+
+
+let keys= Object.keys(car)
+console.log(keys)
+
+let values = Object.values(car)
+console.log(values)
