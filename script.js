de0bcc77f7377a6b55e68cfb7f8b29aafc0d30da
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2024-09-23 18:49:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,65 @@
-let car =  {
-    make : "Toyota", 
-    model: "RAV 4", 
-    year : 2021, 
-    start: function(){
-        console.log("The Car is starting")
-    }, 
-    getDetails: function(){
+class Car {
+
+    constructor(pmake, pmodel, pyear){
+
+        this.make = pmake
+        this.model = pmodel
+        this.year = pyear
+
+    }
+
+    getDetails(){
         return `The car make is ${this.make} and the year is ${this.year}`
     }
+
 }
 
-car.start()
+let obj1 = new Car("Nissan", "Murano", 2023)
+let obj2 = new Car("Toyota", "RAV 4", 2021)
 
-console.log(car.getDetails())
-
-
-let keys= Object.keys(car)
-console.log(keys)
-
-let values = Object.values(car)
-console.log(values)
+console.log(obj1.getDetails())
+console.log(obj2.getDetails())
 
 
+class Person {
+
+    constructor ( pname , page ) {
+        this.name = pname
+        this.age = page
+    }
+
+    introduce(){
+        return `Hi, I´m ${this.name}  and I´m ${this.age} years old`
+    }
+
+}
+
+
+let per1 = new Person("Patrick", 29)
+let per2 = new Person("Julie", 24)
+let per3 = new Person("PAX", 31)
+let per4 = new Person("Fernando", 35)
+
+let dataPerson = []
+dataPerson.push(per1)
+dataPerson.push(per2)
+dataPerson.push(per3)
+dataPerson.push(per4)
+
+console.log("For Loop")
+
+for (let i=0; i < dataPerson.length; i++){
+   let item  = dataPerson[i]
+   console.log(item.introduce)
+}
+
+
+// forEach 
+
+console.log("For Each")
+
+dataPerson.forEach( (item)=>{
+
+    console.log(item.introduce())
+
+})
